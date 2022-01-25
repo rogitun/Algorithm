daea2adf49df2fdfe9968d03378641b023b1dca7
rogitun <rogitun@gmail.com>
2022-01-25 23:43:08 +0900

    "sds_lecture_7th_day"

---
+++ src/day_7th/Boj_1854.java
@@ -0,0 +1,32 @@
+package day_7th;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_1854 {
+    static int n;
+    static int m;
+    static int k;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+        }
+
+
+    }
+}
