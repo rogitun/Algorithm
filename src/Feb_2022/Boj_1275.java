ea3d40d4fb107709155da3821a5c3d2a355f4521
rogitun <rogitun@gmail.com>
2022-02-10 23:47:45 +0900

    "Algorithm_study"

--- src/Feb_2022/Boj_1275.java
+++ src/Feb_2022/Boj_1275.java
@@ -1,8 +1,6 @@
 package Feb_2022;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Boj_1275 {
@@ -14,6 +12,7 @@ public class Boj_1275 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
@@ -46,6 +45,8 @@ public class Boj_1275 {
             //System.out.println( "chgnum "+changeNumber);
             update(1,S,1,a,b-changeNumber);
             System.out.println(result);
+            //bw.write(result + "\n");
+            //bw.flush();
             //System.out.println("=======================");
         }
 
---
+++ src/Feb_2022/Boj_14499.java
@@ -0,0 +1,111 @@
+package Feb_2022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_14499 {
+    static int N; //세로
+    static int M; //가로
+
+    static int x;
+    static int y;
+
+    static int[][] map;
+    static int[] dice = new int[7];
+    static int[] prior = new int[7];
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); //세로
+        M = Integer.parseInt(st.nextToken()); //가로
+        y = Integer.parseInt(st.nextToken())+1;
+        x = Integer.parseInt(st.nextToken())+1;
+        int K = Integer.parseInt(st.nextToken()); //명령의 개수
+
+        map = new int[N+1][M+1];
+
+        for(int i=1;i<=N;i++) { //지도 초기화
+            st = new StringTokenizer(br.readLine());
+            for(int j=1;j<=M;j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        //dice 는 1번이 top 6번이 bottom이다.
+        st = new StringTokenizer(br.readLine()); //이동 명령
+        for(int i=0;i<K;i++) {
+            int next = Integer.parseInt(st.nextToken());
+            move(next); //주사위의 방향 이동시키고 지도의 숫자를 bottom 에 넣는다.
+        }
+
+    }
+    private static void move(int next) {
+        if(next == 1 && x+1 <= M)//동쪽
+        {
+            dice[3] = prior[1];
+            dice[1] = prior[4];
+            dice[6] = prior[3];
+            dice[4] = prior[6];
+            if(map[y][x+1] ==0) {
+                map[y][x+1] = dice[6];
+            }
+            else {
+                dice[6] = map[y][x+1];
+                map[y][x+1] = 0;
+            }
+            x++;
+            prior = dice.clone();
+            System.out.println(dice[1]);
+        }
+        else if(next == 2 && x-1 > 0) { //서쪽
+            dice[4] = prior[1];
+            dice[1] = prior[3];
+            dice[3] = prior[6];
+            dice[6] = prior[4];
+            if(map[y][x-1] ==0) {
+                map[y][x-1] = dice[6];
+            }
+            else {
+                dice[6] = map[y][x-1];
+                map[y][x-1] = 0;
+            }
+            x--;
+            prior = dice.clone();
+            System.out.println(dice[1]);
+        }
+        else if(next == 3 && y-1 > 0) { //북쪽
+            dice[2] = prior[1];
+            dice[1] = prior[5];
+            dice[5] = prior[6];
+            dice[6] = prior[2];
+            if(map[y-1][x] ==0) {
+                map[y-1][x] = dice[6];
+            }
+            else {
+                dice[6] = map[y-1][x];
+                map[y-1][x] = 0;
+            }
+            y--;
+            prior = dice.clone();
+            System.out.println(dice[1]);
+        }
+        else if(next == 4 && y+1 <= N) { //남쪽
+            dice[2] = prior[6];
+            dice[1] = prior[2];
+            dice[5] = prior[1];
+            dice[6] = prior[5];
+            if (map[y + 1][x] == 0) {
+                map[y + 1][x] = dice[6];
+            } else {
+                dice[6] = map[y + 1][x];
+                map[y + 1][x] = 0;
+            }
+            y++;
+            prior = dice.clone();
+            System.out.println(dice[1]);
+        }
+    }
+}
