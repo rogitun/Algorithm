d0ff42e93b037cf12806846d9d22296c246d4b36
rogitun <rogitun@gmail.com>
2022-02-11 21:32:21 +0900

    "Algorithm_study"

--- src/Feb_2022/Boj_14499.java
+++ src/Feb_2022/Boj_14499.java
@@ -3,6 +3,8 @@ package Feb_2022;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Boj_14499 {
@@ -15,7 +17,6 @@ public class Boj_14499 {
     static int[][] map;
     static int[] dice = new int[7];
     static int[] prior = new int[7];
-
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
