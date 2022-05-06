c7f4aa5d500383ddad72546be07a667e6003cc71
rogitun <rogitun@gmail.com>
2022-05-07 01:07:43 +0900

    "Algorithm_study"

---
+++ src/TT5_MAY/Pgs_완주하지못한선수.java
@@ -0,0 +1,23 @@
+package TT5_MAY;
+
+import java.util.HashMap;
+
+public class Pgs_완주하지못한선수 {
+
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String,Integer> comp = new HashMap<>();
+        for (String s : completion) {
+            comp.put(s,comp.getOrDefault(s,0)+1);
+        }
+        for (String s : participant) {
+            Integer orDefault = comp.getOrDefault(s, 0);
+            if(orDefault>0){
+                comp.replace(s, comp.get(s)-1);
+            }
+            if(orDefault==0)
+                answer=s;
+        }
+        return answer;
+    }
+}
