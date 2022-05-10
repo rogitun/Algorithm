6901b28f0bb2fd5bc48b72e3d3026b7559aec1f7
rogitun <rogitun@gmail.com>
2022-05-10 22:25:15 +0900

    "Algorithm_study"

---
+++ src/TT5_MAY/Pgs_위장.java
@@ -0,0 +1,18 @@
+package TT5_MAY;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Pgs_위장 {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        }
+        int sum = 1;
+        for (Integer value : map.values()) {
+            sum *= (value + 1);
+        }
+        return sum - 1;
+    }
+}
