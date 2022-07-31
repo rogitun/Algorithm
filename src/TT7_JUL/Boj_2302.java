0372baef6efcc64d201f0f68e877c933c1d90263
rogitun <rogitun@gmail.com>
2022-08-01 03:41:07 +0900

    Algorithm_dp

---
+++ src/TT7_JUL/Boj_2302.java
@@ -0,0 +1,33 @@
+package TT7_JUL;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_2302 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        int result = 1;
+        int tmp = 0;
+
+        for (int i = 0; i < M; i++) {
+            int idx = Integer.parseInt(br.readLine());
+            result *= dp[idx - tmp - 1];
+            tmp = idx;
+        }
+        result *= dp[N - tmp];
+        System.out.println(result);
+
+    }
+}
